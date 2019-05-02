738d4909420134c87df9b3b41f06154476928ce0
ptamg <ptamganesh@gmail.com>
2019-05-02 12:25:39 +0000

    added multi operation

--- tfairops.py
+++ tfairops.py
@@ -7,7 +7,7 @@ t1 = tf.constant([[5, 10, 15], [4, 8, 12], [6, 12, 18]])
 t2 = tf.constant([[5, 5, 5], [2, 2, 2], [3, 3, 3]])
 tensor_sum = tf.add(t1, t2)  # Add two tensors.
 tensor_sub = tf.subtract(t1, t2)  # Subtract two tensors.
-#tensor_mul = tf.multiply(t1, t2)  # Multiples two tensors.
+tensor_mul = tf.multiply(t1, t2)  # Multiples two tensors.
 #tensor_divide = tf.divide(t1, t2)  # Divides two tensors without roundoff.
 #tensor_div = tf.div(t1, t2)  # Divides two tensors with roundoff.
 #tensor_mod = tf.div(t1, t2)  # Find reminder the modulo operation.
@@ -18,8 +18,8 @@ print("\n----------- SUM ----------------\n")
 print(sess.run(tensor_sum))
 print("\n------------ SUBTRACT ----------\n")
 print(sess.run(tensor_sub))
-#print("\n------------ MULTIPLY ----------\n")
-#print(sess.run(tensor_mul))
+print("\n------------ MULTIPLY ----------\n")
+print(sess.run(tensor_mul))
 #print("\n------------ DIVIDE ------------\n")
 #print(sess.run(tensor_divide))
 #print("\n------------ DIVIDE ------------\n")
