b9815bab8ffa60ee705d2875b9494ebf22e2e98a
ptamg <ptamganesh@gmail.com>
2019-05-02 12:07:19 +0000

    added substraction operation

--- tfairops.py
+++ tfairops.py
@@ -6,7 +6,7 @@ import tensorflow as tf
 t1 = tf.constant([[5, 10, 15], [4, 8, 12], [6, 12, 18]])
 t2 = tf.constant([[5, 5, 5], [2, 2, 2], [3, 3, 3]])
 tensor_sum = tf.add(t1, t2)  # Add two tensors.
-#tensor_sub = tf.subtract(t1, t2)  # Subtract two tensors.
+tensor_sub = tf.subtract(t1, t2)  # Subtract two tensors.
 #tensor_mul = tf.multiply(t1, t2)  # Multiples two tensors.
 #tensor_divide = tf.divide(t1, t2)  # Divides two tensors without roundoff.
 #tensor_div = tf.div(t1, t2)  # Divides two tensors with roundoff.
@@ -16,8 +16,8 @@ print(sess.run(t1))
 print(sess.run(t2))
 print("\n----------- SUM ----------------\n")
 print(sess.run(tensor_sum))
-#print("\n------------ SUBTRACT ----------\n")
-#print(sess.run(tensor_sub))
+print("\n------------ SUBTRACT ----------\n")
+print(sess.run(tensor_sub))
 #print("\n------------ MULTIPLY ----------\n")
 #print(sess.run(tensor_mul))
 #print("\n------------ DIVIDE ------------\n")
